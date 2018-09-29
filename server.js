3e08712ba39061702a4dc7adf77db696d00ebc89
Jim <jim_lubbers@me.com>
2018-09-29 15:50:16 +0200

    Github commit test 2

--- server.js
+++ server.js
@@ -35,6 +35,10 @@ hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 });
 
+const test = () => {
+
+};
+
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
